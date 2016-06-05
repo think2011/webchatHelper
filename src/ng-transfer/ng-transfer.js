652f6d43ec0504c8a643356cdf8ae8770a5fd16b
think2011 <452125301.hzplay@gmail.com>
2016-06-05 12:14:11 +0800

    feat 分组

--- src/ng-transfer/ng-transfer.js
+++ src/ng-transfer/ng-transfer.js
@@ -5,16 +5,18 @@ class Ctrl {
     constructor($scope) {
         $scope.ctrl = this
 
+        this.initList()
+        this.groups = this.fetchGroups()
+        this.tab    = 1
+    }
+
+    initList() {
         this.from = {
             items: angular.copy(tools.fetchAllContacts())
         }
         this.to   = {
             items: []
         }
-
-        this.tab = 0
-
-        this.groups = []
     }
 
     transfer(fromItems, toItems) {
@@ -29,6 +31,52 @@ class Ctrl {
 
     hasSomeChecked(items) {
         return items.some((item) => item.checked)
+    }
+
+    saveGroups() {
+        let name = prompt('给分组取个名字：')
+
+        if (!name) return
+
+        this.groups.unshift({
+            name,
+            items: this.to.items
+        })
+
+        this.writeGroups()
+
+        alert('保存完毕')
+    }
+
+    selectGroup(group) {
+        this.initList()
+
+        this.from.items.forEach((item) => {
+            item.checked = group.some((groupItem) => groupItem.UserName === item.UserName)
+        })
+
+        this.transfer(this.from.items, this.to.items)
+        this.tab = 0
+    }
+
+    delGroup(group) {
+        window.event.stopPropagation()
+        window.event.preventDefault()
+
+        this.groups = this.groups.filter((item) => item.name !== group.name)
+
+        this.writeGroups()
+    }
+
+
+    fetchGroups() {
+        return JSON.parse(localStorage.wechatHelperGroups || '[]')
+    }
+
+    writeGroups() {
+        let groups = this.groups
+
+        return localStorage.wechatHelperGroups = JSON.stringify(groups)
     }
 }
 
